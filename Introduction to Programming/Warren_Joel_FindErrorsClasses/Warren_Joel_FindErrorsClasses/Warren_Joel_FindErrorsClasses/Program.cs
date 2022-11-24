442cef4d9e6523bb75019da8547429ed4a0d9c3a
Joel Warren <72631943+joeltwarren@users.noreply.github.com>
2022-11-24 13:17:36 -0500

    Fixing Broken Classes Exercise

---
+++ Introduction to Programming/Warren_Joel_FindErrorsClasses/Warren_Joel_FindErrorsClasses/Warren_Joel_FindErrorsClasses/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindErrorsClasses
+{
+    class Program
+    {
+
+        /*
+         * Joel Warren
+         * 4-23-18
+         * Assignment -> FindErrorsClasses
+         * 
+         */
+        static void Main(string[] args)
+        {
+            Theater AmcCineplex20 = new Theater("AMC Cineplex 20", 20, 10.00m);
+            Theater RegalCinema = new Theater("Regal Cinema", 15, 8.00m);
+
+            Console.WriteLine("\r\nLet's go see a movie at {0}!\r\nThey have {1} movies to choose from and the average ticket price is {2}.", AmcCineplex20.GetName(), AmcCineplex20.GetNumScreens(), AmcCineplex20.GetTicketPrice().ToString("C"));
+            Console.WriteLine("\r\nWhat about {0} instead?\r\nTheir average ticket price is only {1}.\r\nThe only drawback is that they only have {2} screens.", RegalCinema.GetName(), RegalCinema.GetTicketPrice().ToString("C"), RegalCinema.GetNumScreens());
+
+
+            decimal totalRegal = RegalCinema.TotalTicketCost(4);
+
+            Console.WriteLine("\r\nIf all 4 of us go to {0}, then that would bring the total cost to {1}.", RegalCinema.GetName(), totalRegal.ToString("C"));
+            Console.WriteLine("\r\nWait, I forgot I have a coupon for $3.00 off a movie at the {0}!", AmcCineplex20.GetName());
+
+            
+            AmcCineplex20.SetTicketPrice(AmcCineplex20.GetTicketPrice() - 3.00m);
+
+            Console.WriteLine("\r\nThat would make a ticket there cost only {0}.", AmcCineplex20.GetTicketPrice().ToString("C"));
+
+            decimal totalAMC = AmcCineplex20.TotalTicketCost(4);
+
+            Console.WriteLine("\r\nWith your coupon, all 4 of us can go for only {0}!\r\nLet's go to {1}", totalAMC.ToString("C"), AmcCineplex20.GetName());
+
+        }
+    }
+}
---
+++ Introduction to Programming/Warren_Joel_FindErrorsClasses/Warren_Joel_FindErrorsClasses/Warren_Joel_FindErrorsClasses/Theater.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindErrorsClasses
+{
+    class Theater
+    {
+        //Member Variables
+        string mTheaterName;
+        int mNumberOfScreens;
+        decimal mAverageTicketPrice;
+
+        //Create the constructor function
+        public Theater(string _theaterName, int _numberOfScreens, decimal _averageTicketPrice)
+        {
+
+            //Use the incomming parameters to inialize our original member variables
+            mTheaterName = _theaterName;
+            mNumberOfScreens = _numberOfScreens;
+            mAverageTicketPrice = _averageTicketPrice;
+            
+
+        }
+
+        //Getters
+        public string GetName()
+        {
+            return mTheaterName;
+        }
+
+        public int GetNumScreens()
+        {
+            return  mNumberOfScreens;
+        }
+
+        public decimal GetTicketPrice()
+        {
+            return mAverageTicketPrice;
+        }
+
+        //Settters
+        public void SetTitle(string _theaterName)
+        {
+            this.mTheaterName = _theaterName;
+        }
+
+        public void SetNumScreens(int _numScreens)
+        {
+            this.mNumberOfScreens = _numScreens;
+        }
+
+        public void SetTicketPrice(decimal _ticketPrice)
+        {
+            this.mAverageTicketPrice = _ticketPrice;
+        }
+
+
+        //Custom function to return how much #number of tickets will be
+        public decimal TotalTicketCost(int _numberOfTickets)
+        {
+            decimal totalCost = _numberOfTickets * mAverageTicketPrice;
+
+            return totalCost;
+
+        }
+    }
+}
